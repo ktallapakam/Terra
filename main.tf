da8a63117b0735a229a0050ad362cd4677ef3c98
Kishore <ktallapakam@gmail.com>
2025-06-05 14:21:35 +0530

    fupdated

--- main.tf
+++ main.tf
@@ -1,15 +1,9 @@
 provider "aws" {
-  region = "ap-southeast-2"
-}
-
-resource "aws_instance" "testServer-01" {
-    ami             = "ami-084568db4383264d4"
-    instance_type   = "t2.micro"
-    subnet_id       = "subnet-04cc49f127f01874e"
+  region = "us-east-1"
 }
 
 resource "aws_s3_bucket" "tf_state" {
-  bucket = "my-terraform-state-bucket-123456"  # must be globally unique
+  bucket = "05062024-terraform-state-bucket-123456"  # must be globally unique
   force_destroy = true
 }
 
@@ -26,10 +20,17 @@ resource "aws_dynamodb_table" "tf_locks" {
 # --------------------------
 terraform {
   backend "s3" {
-    bucket         = "my-terraform-state-bucket-123456"  # Same as the bucket above
+    bucket         = "my-terraform-state-bucket-05062024"  # Same as the bucket above
     key            = "env/dev/terraform.tfstate"
-    region         = "ap-southeast-2"
+    region         = "us-east-1"
     dynamodb_table = "terraform-locks"
     encrypt        = true
   }
+}
+
+
+resource "aws_instance" "testServer-01" {
+    ami             = "ami-084568db4383264d4"
+    instance_type   = "t2.micro"
+    subnet_id       = "subnet-04cc49f127f01874e"
 }
