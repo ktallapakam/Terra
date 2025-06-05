2f8034c1a4c2e5fd68ca514eb78e11775ee25856
Kishore <ktallapakam@gmail.com>
2025-06-05 12:59:22 +0530

    added main.tf

---
+++ main.tf
@@ -0,0 +1,9 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "TestInstance-01" {
+    ami             = "ami-084568db4383264d4"
+    instance_type   = "t2.micro"
+    subnet_id       = "subnet-04cc49f127f01874e"
+}
